957a6f9e8d67487a13fec8e62b602ef05e218617
George Spanos <gspanos@moby-it.com>
2024-05-26 00:19:09 +0300

    add jest test

---
+++ lua/plugins/test.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "haydenmeade/neotest-jest",
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "haydenmeade/neotest-jest" },
+    opts = function(_, opts)
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "npm test --",
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        })
+      )
+    end,
+  },
+}
