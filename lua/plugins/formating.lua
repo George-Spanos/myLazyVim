defe9f94ef48379b9d6b268f19ddc88fd486cdc7
George Spanos <gspanos@moby-it.com>
2024-05-27 13:20:25 +0300

    changing theme and updates

--- lua/plugins/formating.lua
+++ lua/plugins/formating.lua
@@ -1,29 +1,28 @@
 return {
   "stevearc/conform.nvim",
-  opts = function()
-    return {
-      formatters_by_ft = {
-        haskell = { 'ormolu' }
+  opts = function(_, opts)
+    table.insert(opts.formatters_by_ft, {
+      haskell = { "ormolu" },
+    })
+    table.insert(opts.formatters, {
+      prettier = {
+        require_cwd = true,
+        cwd = require("conform.util").root_file({
+          ".prettierrc",
+          ".prettierrc.json",
+          ".prettierrc.yml",
+          ".prettierrc.yaml",
+          ".prettierrc.json5",
+          ".prettierrc.js",
+          ".prettierrc.cjs",
+          ".prettierrc.mjs",
+          ".prettierrc.toml",
+          "prettier.config.js",
+          "prettier.config.cjs",
+          "prettier.config.mjs",
+        }),
       },
-      formatters = {
-        prettier = {
-          require_cwd = true,
-          cwd = require("conform.util").root_file({
-            ".prettierrc",
-            ".prettierrc.json",
-            ".prettierrc.yml",
-            ".prettierrc.yaml",
-            ".prettierrc.json5",
-            ".prettierrc.js",
-            ".prettierrc.cjs",
-            ".prettierrc.mjs",
-            ".prettierrc.toml",
-            "prettier.config.js",
-            "prettier.config.cjs",
-            "prettier.config.mjs",
-          }),
-        },
-      },
-    }
+    })
+    return opts
   end,
 }
