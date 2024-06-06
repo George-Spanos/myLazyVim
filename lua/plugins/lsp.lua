624f7f35251fb5d3fee676ce5698356e901c86f7
George Spanos <gspanos@moby-it.com>
2024-06-06 16:08:23 +0300

    add deno

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,5 @@
+local nvim_lsp = require("lspconfig")
+
 return {
   "neovim/nvim-lspconfig",
   event = "LazyFile",
@@ -6,6 +8,11 @@ return {
     "williamboman/mason-lspconfig.nvim",
   },
   opts = {
+    servers = {
+      denols = {
+        root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+      },
+    },
     inlay_hints = {
       enabled = false,
     },
