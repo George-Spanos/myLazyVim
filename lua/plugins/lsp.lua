a5d811b4043a694c67580b3fb102f5c53cb738f4
George Spanos <gspanos@moby-it.com>
2024-06-06 13:37:31 +0300

    disable inlay hints

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+  "neovim/nvim-lspconfig",
+  event = "LazyFile",
+  dependencies = {
+    "mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  },
+  opts = {
+    inlay_hints = {
+      enabled = false,
+    },
+  },
+}
